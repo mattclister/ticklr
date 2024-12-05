77136b632450954b0322b8e1396a23e4d5cee0f3
Matt Lister <matt.c.lister@gmail.com>
2024-12-05 21:07:07 +0000

    keys moved to environment variables and send email function started

---
+++ ticklr/server/scheduled_functions.js
@@ -0,0 +1,63 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./ticklerDB.db");
+
+const sendEmails = async () => {
+  const getRecordsToEmail = () => {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT reminders.*, users.reminder_email 
+FROM reminders
+JOIN users ON reminders.fk_user_id = users.pk_user_id
+WHERE reminder_date < DATETIME('now');
+`;
+
+      db.all(query, [], (err, rows) => {
+        if (err) {
+          console.error("Database query failed:", err.message);
+          reject(err);
+        } else {
+          console.log("Rows found:", rows);
+          resolve(rows);
+        }
+      });
+    });
+  };
+
+  try {
+    const records = await getRecordsToEmail();
+    if (records.length > 0) {
+      console.log(`Found ${records.length} records to email:`);
+
+      for (const record of records) {
+        console.log(
+          `Sending email to: ${record.pk_reminder_id} ${record.reminder_email} ${record.title}`
+        );
+
+        const formData = require("form-data");
+        const Mailgun = require("mailgun.js");
+        const mailgun = new Mailgun(formData);
+        const mg = mailgun.client({
+          username: "api",
+          key: process.env.MAILGUN_API_KEY})
+
+        mg.messages
+          .create("sandbox-123.mailgun.org", {
+            from: "Excited User <mailgun@ticklr.app>",
+            to: ["test@example.com"],
+            subject: "Hello",
+            text: "Testing some Mailgun awesomeness!",
+            html: "<h1>Testing some Mailgun awesomeness!</h1>",
+          })
+          .then((msg) => console.log(msg)) // logs response data
+          .catch((err) => console.log(err)); // logs any error
+      }
+    } else {
+      console.log("No records found to email.");
+    }
+  } catch (error) {
+    console.error("Error retrieving records:", error.message);
+  }
+};
+
+module.exports = {
+  sendEmails,
+};
---
+++ ticklr/server/server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const bcrypt = require('bcrypt');
+const {createUser, loginUser, validateToken, getReminders, addReminder, updateSettings} = require('./queries')
+const cron = require('node-cron');
+const {sendEmails} = require('./scheduled_functions')
+const readline = require('readline');
+require('dotenv').config();
+const mailgun_api_key = process.env.MAILGUN_API_KEY
+
+
+// Command Console Connection
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  rl.on('line', (input) => {
+    if (input.trim() === 'send-emails') {
+      console.log('Manual email trigger received via CLI.');
+      sendEmails();
+    } else {
+      console.log(`Unknown command: ${input}`);
+    }
+  });
+
+// Tell express to parse json when request header says body is json
+app.use(express.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// -- Requests --
+
+// Create User
+app.post('/users', createUser)
+
+// Login User
+app.post('/login', loginUser);
+
+// Authenticate User
+app.post('/authenticate', validateToken);
+
+// Get reminders
+app.get("/reminders", getReminders);
+
+// Add reminder
+app.post("/reminders", addReminder);
+
+// Update settings
+app.post("/settings", updateSettings);
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+// Scheduled Operations
+
+cron.schedule('0 * * * *', () => {
+    console.log('Preparing emails...');
+    sendEmails();
+  });
