09240ec1eadfc31c46225122166a8f8ed1ee33ca
Matt Lister <matt.c.lister@gmail.com>
2024-12-06 13:19:32 +0000

    email api set up. not yet running the full loop or scheduling

--- ticklr/server/scheduled_functions.js
+++ ticklr/server/scheduled_functions.js
@@ -1,7 +1,11 @@
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./ticklerDB.db");
+require('dotenv').config();
 
 const sendEmails = async () => {
+
+    // Function to get records to email from db
+
   const getRecordsToEmail = () => {
     return new Promise((resolve, reject) => {
       const query = `SELECT reminders.*, users.reminder_email 
@@ -22,35 +26,38 @@ WHERE reminder_date < DATETIME('now');
     });
   };
 
+      // Get records to email from db
+
+              // for (const record of records) {
+        //     console.log(
+        //       `Sending email to: ${record.pk_reminder_id} ${record.reminder_email} ${record.title}`
+        //     );
+
   try {
     const records = await getRecordsToEmail();
     if (records.length > 0) {
       console.log(`Found ${records.length} records to email:`);
-
-      for (const record of records) {
-        console.log(
-          `Sending email to: ${record.pk_reminder_id} ${record.reminder_email} ${record.title}`
-        );
 
         const formData = require("form-data");
         const Mailgun = require("mailgun.js");
         const mailgun = new Mailgun(formData);
         const mg = mailgun.client({
           username: "api",
-          key: process.env.MAILGUN_API_KEY})
+          key: process.env.MAILGUN_SENDING_API_KEY,
+          url: 'https://api.eu.mailgun.net'
+        });
 
         mg.messages
-          .create("sandbox-123.mailgun.org", {
-            from: "Excited User <mailgun@ticklr.app>",
-            to: ["test@example.com"],
-            subject: "Hello",
-            text: "Testing some Mailgun awesomeness!",
-            html: "<h1>Testing some Mailgun awesomeness!</h1>",
+          .create("ticklr.app", {
+            from: "Reminders <reminders@ticklr.app>",
+            to: ["Inbox <add.task.axvv0puffgurtyvk@todoist.net>"],
+            subject: "New Test Task",
+            text: "Testing adding a task to todoist",
+            html: "<p>Testing adding a task to todoist html</p>",
           })
           .then((msg) => console.log(msg)) // logs response data
           .catch((err) => console.log(err)); // logs any error
-      }
-    } else {
+      } else {
       console.log("No records found to email.");
     }
   } catch (error) {
