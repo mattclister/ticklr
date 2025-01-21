e382d8d5edd2bf3de0437c234ea99d8b762a9267
Matt Lister <matt.c.lister@gmail.com>
2025-01-21 21:18:17 +0000

    Trying dropdown menu

--- client/src/Components/RemindersList.tsx
+++ client/src/Components/RemindersList.tsx
@@ -1,30 +1,42 @@
 import { useState, useEffect } from "react";
 import { getReminders } from "../Utilities/ServerRequests";
 import { ReminderType } from "../Utilities/types";
-import dayjs from 'dayjs';
+import dayjs from "dayjs";
 import React from "react";
+import { notify } from "../Utilities/notifications";
 
 interface Props {
   setBottomBarVisible: React.Dispatch<React.SetStateAction<boolean>>;
   setActive: React.Dispatch<React.SetStateAction<ReminderType | undefined>>;
   active: ReminderType | undefined;
   reminderdata: ReminderType[] | undefined;
-  setReminderData: React.Dispatch<React.SetStateAction<ReminderType[] | undefined>>;
+  setReminderData: React.Dispatch<
+    React.SetStateAction<ReminderType[] | undefined>
+  >;
   setTopBarVisible: React.Dispatch<React.SetStateAction<boolean>>;
   bottomBarVisible: boolean;
-  topBarVisible: boolean}
+  topBarVisible: boolean;
+}
 
-export const RemindersList = ({ setTopBarVisible, setBottomBarVisible, setActive, active, reminderdata, setReminderData, bottomBarVisible, topBarVisible}: Props) => {
+export const RemindersList = ({
+  setBottomBarVisible,
+  setActive,
+  active,
+  reminderdata,
+  setReminderData,
+  bottomBarVisible,
+  topBarVisible,
+}: Props) => {
   const [loading, setLoading] = useState<boolean>(true);
   const activehandler = (reminderdata: ReminderType[], id: Number) => {
-    setActive(reminderdata.filter((item) => item.pk_reminder_id === id)[0])
-  }
+    setActive(reminderdata.filter((item) => item.pk_reminder_id === id)[0]);
+  };
 
   useEffect(() => {
     const fetchReminders = async () => {
       try {
         console.log(`Fetching Reminders...`);
-        const requestedData:ReminderType[] = await getReminders();
+        const requestedData: ReminderType[] = await getReminders();
         setReminderData(requestedData);
         setLoading(false);
       } catch (error) {
@@ -33,62 +45,119 @@ export const RemindersList = ({ setTopBarVisible, setBottomBarVisible, setActive
       }
     };
     fetchReminders();
-
   }, []);
 
   const onAddNew = () => {
-    setActive(undefined)
-    setBottomBarVisible(true)
-  }
+    setActive(undefined);
+    setBottomBarVisible(true);
+  };
 
-  if(loading){return <p>loading...</p>} else {
-
-  return (
-    <>
-      {reminderdata === undefined ? (
-        <li className="list-group-item">
-          {" "}
-          <button
-            id="add-new-item-btn"
-            onClick={() => setBottomBarVisible(true)}
-            className="btn btn-outline-secondary"
-          >
-            + add new item
-          </button>
-        </li>
-      ) : (
-        <ul id="reminders-list" className="list-group" >
-          {reminderdata.sort((a,b)=>dayjs(a.reminder_date).valueOf() - dayjs(b.reminder_date).valueOf()).map((item) => (
-            <li
-              className={`list-group-item${item.pk_reminder_id === active?.pk_reminder_id ? " selected" : ""}`}
-              key={item.pk_reminder_id.toString()}
-              onClick={() => {
-                console.log(item.pk_reminder_id)
-                activehandler(reminderdata,item.pk_reminder_id)}}
-            >
-              <div className="row align-items-start">
-                <div className="col">
-                  <h6>{dayjs(item.reminder_date as string).format('DD-MMM')}</h6>
-                </div>
-                <div className="col">{item.title.length>40?item.title.slice(0,40)+"...":item.title}</div>
-                <div className="col">{item.unit_count && item.unit_time? `${item.unit_count} ${item.unit_time}s`:""}</div>
-              </div>
-            </li>
-          ))}
-          <li id="add-new-item-row" className="list-group-item no-highlight">
+  if (loading) {
+    return <p>loading...</p>;
+  } else {
+    return (
+      <>
+        {reminderdata === undefined ? (
+          <li className="list-group-item">
             {" "}
             <button
               id="add-new-item-btn"
-              onClick={() => onAddNew()}
+              onClick={() => setBottomBarVisible(true)}
               className="btn btn-outline-secondary"
             >
               + add new item
             </button>
           </li>
-          {bottomBarVisible || topBarVisible?<li className="list-group-item no-highlight" style={{borderColor: "transparent"}}><div id="blank_div" style={{height: "55vh", maxHeight: "430px", minHeight: "400px"}}></div></li>: null}
-        </ul>   
-      )}
-    </>
-  );
-};
+        ) : (
+          <ul id="reminders-list" className="list-group">
+            {reminderdata
+              .sort(
+                (a, b) =>
+                  dayjs(a.reminder_date).valueOf() -
+                  dayjs(b.reminder_date).valueOf()
+              )
+              .map((item) => (
+                <li
+                  className={`list-group-item${
+                    item.pk_reminder_id === active?.pk_reminder_id
+                      ? " selected"
+                      : ""
+                  }`}
+                  key={item.pk_reminder_id.toString()}
+                  onClick={() => {
+                    console.log(item.pk_reminder_id);
+                    activehandler(reminderdata, item.pk_reminder_id);
+                  }}
+                >
+                  <div className="row align-items-start">
+                    <div className="col">
+                      <h6>
+                        {dayjs(item.reminder_date as string).format("DD-MMM")}
+                      </h6>
+                    </div>
+                    <div className="col">
+                      {item.title.length > 40
+                        ? item.title.slice(0, 40) + "..."
+                        : item.title}
+                    </div>
+                    <div className="col">
+                      {item.unit_count && item.unit_time
+                        ? `${item.unit_count} ${item.unit_time}s`
+                        : ""}
+                    </div>
+                    <div className="col">
+                    <div className="dropdown">
+                      <button
+                        className="btn btn-secondary dropdown-toggle"
+                        type="button"
+                        id="dropdownMenuButton"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        ...
+                      </button>
+                      <div
+                        className="dropdown-menu"
+                        aria-labelledby="dropdownMenuButton"
+                      >
+                        <a className="dropdown-item" onClick={()=>notify("Deleted","success")}>
+                          Delete
+                        </a>
+                      </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            <li id="add-new-item-row" className="list-group-item no-highlight">
+              {" "}
+              <button
+                id="add-new-item-btn"
+                onClick={() => onAddNew()}
+                className="btn btn-outline-secondary"
+              >
+                + add new item
+              </button>
+            </li>
+            {bottomBarVisible || topBarVisible ? (
+              <li
+                className="list-group-item no-highlight"
+                style={{ borderColor: "transparent" }}
+              >
+                <div
+                  id="blank_div"
+                  style={{
+                    height: "55vh",
+                    maxHeight: "430px",
+                    minHeight: "400px",
+                  }}
+                ></div>
+              </li>
+            ) : null}
+          </ul>
+        )}
+      </>
+    );
+  }
 };
